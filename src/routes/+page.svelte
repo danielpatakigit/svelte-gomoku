20a46d713d6bee9b34714cb2d5b70f762e760c1a
Dániel Pataki <e12223375@student.tuwien.ac.at>
2023-10-07 22:24:16 +0200

    v1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Scoreboard from "./Scoreboard.svelte";
+  import Board from "./Board.svelte";
+
+  let boardSize = 10;
+  let turn = 1; // Define 'turn' before using it
+  $: boardArray = new Array(boardSize).fill([]).map(() => new Array(boardSize).fill(null));
+  let currentPlayer = 1;
+  $: state = { boardSize, boardArray, currentPlayer, turn };
+</script>
+
+<h1>Five in a Row - Gomoku</h1>
+<span>{state.currentPlayer}</span>
+<h2>Current Player {currentPlayer}</h2>
+<div>
+  <label for="board-size">Please enter board size!</label>
+  <input id="board-size" type="range" bind:value={boardSize} min="10" max="20" />
+  <span>Selected board size: {boardSize}</span>
+</div>
+<div class="">
+  <Scoreboard />
+  <Board {state} />
+</div>
+
+<style>
+</style>
---
+++ src/routes/Scoreboard.svelte
@@ -0,0 +1,7 @@
+<script>
+</script>
+
+<div class="scoreboard" />
+
+<style>
+</style>
